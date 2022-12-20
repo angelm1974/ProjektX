8d5b2287b8ff606e39890366156702cfe715a1b2
angelm1974b <mariusz980@gmail.com>
2022-12-20 13:23:30 +0100

    AplikacjaX

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import {StyleSheet, Text, Image, View,Button,TextInput  } from 'react-native';
 
 export default function App() {
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>Witaj w aplikacji listy!</Text>
+      </View>
+      <View>      
+        <Image
+        style={styles.tinyLogo}
+        source={require('./assets/sun.png')}
+      />
+      </View>
+      <View style={styles.inputContainer}>
+      <TextInput style={styles.input} placeholder="Wpisz imię" />
+      </View>
+      <View style={styles.buttonsContainer}>
+        <Button title="Anuluj" />
+        <Button title="Dodaj" />
+      </View>
+      
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'column',
+    backgroundColor: 'white',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  inputContainer: {
+    width: '90%',
+  },
+  buttonsContainer: {
+    width: '60%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
