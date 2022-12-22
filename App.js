2d1c0a92af40a5da4618a563f8e86128569b7220
angelm1974b <mariusz980@gmail.com>
2022-12-22 11:06:52 +0100

    po stylowaniu

--- App.js
+++ App.js
@@ -1,35 +1,32 @@
 //Sekcja importów
 //Importujemy biblioteki Reacta, komponenty z biblioteki React Native oraz nasze własne komponenty
 import { useState } from 'react';
-import { StyleSheet, Text, Image, View, Button, TextInput, FlatList, TouchableOpacity, ScrollView } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
 import GoalItem from './components/GoalItem';
 
 //Importujemy nasz komponent Tytul - został wyciiągnięty z głównej funkcji App do oddzielnego pliku
 import Tytul from './components/Tytul';
 import Logo from './components/Logo';
+import GoalInput from './components/GoalInput';
 
 //Główna funkcja aplikacji od niej zaczyna się cała aplikacja
 //W nawiasach kwadratowych podajemy wszystkie komponenty, które chcemy wykorzystać w naszej aplikacji
 //te komponenty nazywamy "hookami"
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState('');
-  const [courseGoals, setCourseGoals] = useState([]);
+
+const [courseGoals, setCourseGoals] = useState([]);
 
 
-  //Funkcja, która będzie wywoływana przy każdej zmianie tekstu w TextInput
-  //W parametrze text znajduje się aktualny tekst wpisany w TextInput
-  //Funkcja ta ustawia stan enteredGoal na aktualny tekst wpisany w TextInput
-  const textHandler = (text) => {
-    setEnteredGoal(text);
-  }
+
+
 
   //Funkcja, która będzie wywoływana przy kliknięciu przycisku "Dodaj"
   //Funkcja ta dodaje do listy celów nowy cel
   //Funkcja po zajęciach została zmodyfikowana tak, aby dodawała równiż do każdego celu 
   //unikalny identyfikator
-  function addGoal() {
+  function addGoal(enteredGoal) {
     setCourseGoals((aktualneCele) =>
-      [...courseGoals, { text: enteredGoal, id: Math.random().toString() }
+      [...aktualneCele, { text: enteredGoal, id: Math.random().toString() }
       ]);
   };
   return (
@@ -46,37 +43,26 @@ export default function App() {
     <View style={styles.container}>
       <Tytul />
       <Logo />
+      <GoalInput onAddGoal={addGoal} />
 
-      {/* Komponent TextInput służy do wyświetlania pola tekstowego, 
-      w którym użytkownik może wpisywać tekst */}
-      <View style={styles.inputContainer}>
-        <TextInput onChangeText={textHandler} style={styles.input} placeholder="Wpisz cel" />
-      </View>
-
-      {/* Komponent Button służy do wyświetlania przycisku,
-      Uwaga w bieżącym widoku są dwa przyciski, które mają różne funkcje
-      w którym użytkownik może kliknąć i wykonać jakąś akcję */}
-      <View style={styles.buttonsContainer}>
-        <Button title="Anuluj" />
-        <Button onPress={addGoal} title="Dodaj" />
-      </View>
 
       {/* Komponent FlatList służy do wyświetlania listy elementów
       W atrybucie data podajemy tablicę, która zawiera elementy, które mają być wyświetlone
       W atrybucie renderItem podajemy funkcję, która będzie wywoływana przy każdym elemencie tablicy
       W atrybucie keyExtractor podajemy funkcję, która będzie wywoływana przy każdym elemencie tablicy
       aby wygenerować unikalny identyfikator dla każdego elementu tablicy */}
+      <View style={styles.GoalsContainer}>
       <FlatList data={courseGoals}
         renderItem={(itemData) => {
           return (
-            <GoalItem text={itemData.item.text}/>
+            <GoalItem text={itemData.item.text} />
           );
         }}
         keyExtractor={(item, index) => {
           return item.id;
         }}
         alwaysBounceVertical={false} style={styles.inputContainer} />
-
+      </View>
     </View>
   );
 }
@@ -86,32 +72,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'white',
-    alignItems: 'center',
-
-    padding: 20,
+    paddingTop: 50,
+    paddingHorizontal: 20,
   },
-  tinyLogo: {
-    width: 50,
-    height: 50,
+  GoalsContainer: {
+    marginTop: 20,
+    flex: 5,
   },
-  inputContainer: {
-    width: '90%',
-  },
-  buttonsContainer: {
-    width: '60%',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    borderColor: 'darkblue',
-    borderRadius: 15,
-    padding: 10,
-  },
-
-});
-
+}); 
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,18 +1,56 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native'
+import React,{useState} from 'react'
 
-const GoalInput = () => {
+const GoalInput = (props) => {
+    const [enteredGoal, setEnteredGoal] = useState('');
+
+  //Funkcja, która będzie wywoływana przy każdej zmianie tekstu w TextInput
+  //W parametrze text znajduje się aktualny tekst wpisany w TextInput
+  //Funkcja ta ustawia stan enteredGoal na aktualny tekst wpisany w TextInput
+    const textHandler = (text) => {
+        setEnteredGoal(text);
+      }
+    
+    function addGoalHandler() {
+        props.onAddGoal(enteredGoal);
+        setEnteredGoal('');
+      }
     return (
         <View>
             <View style={styles.inputContainer}>
                 <TextInput onChangeText={textHandler} style={styles.input} placeholder="Wpisz cel" />
-            </View>
-            <View style={styles.buttonsContainer}>
-                <Button title="Anuluj" />
-                <Button onPress={addGoal} title="Dodaj" />
+                <Button onPress={addGoalHandler} title="Dodaj" />
             </View>
         </View>
     )
 }
 
 export default GoalInput
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+        marginBottom: 24,
+        borderBottomWidth: 1,
+        borderBottomColor: 'darkblue',
+
+      },
+    input: {
+      borderWidth: 1,
+      borderColor: '#ccc',
+      width: '70%',
+      padding: 8,
+      borderRadius: 10,
+      marginRight: 8,
+
+      },
+      buttonsContainer: {
+        width: '60%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+      },
+
+});
